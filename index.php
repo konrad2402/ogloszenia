816ace9ebeee1ca30c0efb0b6837175e4bd3f050
Marek Skiba <skibamarek@gmail.com>
2013-04-03 00:33:37 +0200

    Wyświetlamy testowo kategorie

--- index.php
+++ index.php
@@ -6,13 +6,13 @@ try {
 	$pdo = new PDO($dsn, $user, $password, 
 		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION));
 
-	$ogloszenia = $pdo->query('SELECT * FROM ogloszenia');
-	if($ogloszenia) {
-		foreach($ogloszenia as $ogloszenie) {
-			echo 'ogloszenie';
+	$kategorie = $pdo->query('SELECT * FROM kategorie');
+	if($kategorie) {
+		foreach($kategorie as $kategoria) {
+			echo $kategoria['nazwa'] . '<br />';
 		}
 	}
-	$ogloszenia->closeCursor();
+	$kategorie->closeCursor();
 	
 } catch (PDOException $e) {
 	echo 'Połączenie nie mogło zostać utworzone: ' . $e->getMessage();
